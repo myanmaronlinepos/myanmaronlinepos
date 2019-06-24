a6db15a6e89f54d6eb6e0edf93a670339ebba47c
KyawtSandarAung <kyawtsandar277@gmail.com>
2019-06-24 22:06:36 +0630

    this is inventory table

--- src/app/layout/products/inventory/inventory.component.ts
+++ src/app/layout/products/inventory/inventory.component.ts
@@ -1,4 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material';
+import {MatTableDataSource} from '@angular/material/table';
+export interface PeriodicElement {
+  name: string;
+  no: number;
+  quantity: number;
+  category: string;
+  updatequantity:string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {no: 1, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 2, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 3, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 4, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 5, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 6, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 7, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 8, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 9, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 10, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 11, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 12, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 13, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 14, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 15, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 16, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 17, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 18, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 19, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+  {no: 20, name: 'point',category : 'sunflowerseeds', quantity:1000,updatequantity:''},
+
+];
 
 @Component({
   selector: 'app-inventory',
@@ -7,9 +40,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryComponent implements OnInit {
 
+displayedColumns: string[] = ['no', 'name', 'category', 'quantity','updatequantity'];
+dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+// @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+@ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor() { }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+  
   }
 
 }
+
