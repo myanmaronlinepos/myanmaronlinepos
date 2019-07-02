b694eaff022e00eede8371d613e90be5e53d8d3c
KyawtSandarAung <kyawtsandar277@gmail.com>
2019-07-02 17:35:44 +0630

    edit modified detail form

--- src/app/signup/forms/detail-form/detail-form.component.ts
+++ src/app/signup/forms/detail-form/detail-form.component.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { FormBuilder, FormControl, FormGroup, Validators,FormGroupDirective,NgForm } from '@angular/forms';
 
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid);
-  }
-}
+// export class MyErrorStateMatcher implements ErrorStateMatcher {
+  // isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    // const isSubmitted = form && form.submitted;
+    // return !!(control && control.invalid);
+//   }
+// }
 
 @Component({
   selector: 'app-detail-form',
@@ -17,7 +17,8 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 export class DetailFormComponent implements OnInit {
   showStore: boolean=false;
   detailForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  constructor() { }
+  // [errorStateMatcher]="matcher"
   //  fname = new FormControl('', [Validators.required, Validators.fname]);
 
   // getErrorMessage() {
@@ -29,9 +30,9 @@ export class DetailFormComponent implements OnInit {
   ngOnInit() {
     this.detailForm = new FormGroup({
       // email:new FormControl('', [Validators.email]),
-      fname:new FormControl('', []),
-      lname:new FormControl('', []),
-      PhoneNumber:new FormControl('',[Validators.pattern('/^0[1-9]-[0-9]+$')]),
+      // fname:new FormControl('', []),
+      // lname:new FormControl('', []),
+      // PhoneNumber:new FormControl('',[Validators.pattern('/^0[1-9]-[0-9]+$')]),
       // radioGroup:new FormControl('',[Validators.required])
     });
   }
@@ -41,6 +42,6 @@ export class DetailFormComponent implements OnInit {
   onCheck(event: any){
     this.showStore=false;
   }
-  matcher = new MyErrorStateMatcher();
+  // matcher = new MyErrorStateMatcher();
 
 }
