eb916ea59844bd010bd17ed7e1317cc6beb84402
myanmaronlinepos <myanmaronlinepos@gmail.com>
2019-07-08 16:54:35 +0630

    signup

--- src/app/signup/forms/main-form/main-form.component.ts
+++ src/app/signup/forms/main-form/main-form.component.ts
@@ -1,18 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators,FormGroupDirective,NgForm } from '@angular/forms';
-import { SignupComponent } from '../../signup.component';
-import {Router, RouterLink} from '@angular/router';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, FormGroupDirective, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ErrorStateMatcher } from '@angular/material/core';
-import { User } from 'src/app/share/models/User';
+import { EmitterService } from 'src/app/share/services/emitter.service';
+import { SignupModel } from '../signupModel';
 
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-  const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
-  const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
-
-  return (invalidCtrl || invalidParent);
-}
-}
 @Component({
   selector: 'app-main-form',
   templateUrl: './main-form.component.html',
@@ -20,22 +12,32 @@ isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | nu
 })
 
 export class MainFormComponent {
- 
+
   myForm: FormGroup;
-  
+
 
   matcher = new MyErrorStateMatcher();
-
-  constructor(private formBuilder: FormBuilder) {
+  
+  constructor(
+    private formBuilder: FormBuilder, 
+    private router: Router,
+    private emitterService:EmitterService
+    ) {
     this.myForm = this.formBuilder.group({
-      email: [' ',[Validators.required,Validators.email]],
+      email: [' ', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
       confirmPassword: ['']
     }, { validator: this.checkPasswords });
 
   }
-  onSubmit(){
-    console.log(this.myForm);
+  onSubmit() {
+    const data:SignupModel={
+      user_email: this.myForm.value.email,
+      user_password: this.myForm.value.password
+    };
+    
+     this.emitterService.setData(data);
+     this.router.navigate(['/home/signup/detail'])
   }
 
   checkPasswords(group: FormGroup) { // here we have the 'passwords' group
@@ -43,5 +45,13 @@ export class MainFormComponent {
     let confirmPass = group.controls.confirmPassword.value;
     return pass === confirmPass ? null : { notSame: true }
   }
-  }
+}
 
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
+  }
+}
