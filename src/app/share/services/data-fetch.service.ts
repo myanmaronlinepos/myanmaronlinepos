740e0678afc558026611eae84ee795806688348c
myanmaronlinepos <myanmaronlinepos@gmail.com>
2019-07-09 14:09:58 +0630

    data-fetch-service

--- src/app/share/services/data-fetch.service.ts
+++ src/app/share/services/data-fetch.service.ts
@@ -3,6 +3,8 @@ import { ApiRouteService } from './api-route.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/Product';
+import { Category } from '../models/Category';
+import { City } from '../models/City';
 
 @Injectable({
   providedIn: "root"
@@ -16,4 +18,38 @@ export class DataFetchService {
       this.apiService.createCompleteApiRoute(apiUrl)
     );
   }
+
+  getProduct(productId:string):Observable<Product> {
+    let apiUrl="api/user/get/product";
+    return this.http.get<Product>(
+      this.apiService.createCompleteApiRoute(apiUrl),
+      {
+        params: {
+          product_id: productId
+        }
+      }
+    );
+  }
+
+ getAllCategory():Observable<Category[]> {
+   let apiUrl="api/user/get/categories";
+   return this.http.get<Category[]>(
+     this.apiService.createCompleteApiRoute(apiUrl)
+   );
+ }
+
+getAllCity():Observable<City[]> {
+  let apiUrl="api/guest/allcity";
+  return this.http.get<City[]>(
+    this.apiService.createCompleteApiRoute(apiUrl)
+  );
+}
+
+getCity():Observable<City> {
+  let apiUrl="api/user/get/city";
+  return this.http.get<City>(
+    this.apiService.createCompleteApiRoute(apiUrl)
+  )
+}
+  
 }
