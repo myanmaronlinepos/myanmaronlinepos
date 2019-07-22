5c5ebd000bdd11ea0849aa31b606a2a13863c042
KyawtSandarAung <kyawtsandar277@gmail.com>
2019-07-22 16:59:57 +0630

    signup data validation

--- src/app/signup/forms/detail-form/detail-form.component.ts
+++ src/app/signup/forms/detail-form/detail-form.component.ts
@@ -9,8 +9,6 @@ export interface City {
   id: number;
   viewValue: string;
 }
-
-
 @Component({
   selector: 'app-detail-form',
   templateUrl: './detail-form.component.html',
@@ -19,6 +17,14 @@ export interface City {
 
 export class DetailFormComponent implements OnInit {
 showSpinner=false;
+isValidFormSubmitted = false;
+showStore: boolean = false;
+sellbuy = ['Sell', 'Buy']
+detailForm: FormGroup;
+showProgress:boolean=false;
+mainFormData:SignupModel;
+signupData: User;
+	
 loadData(){
   this.showSpinner=true;
 }
@@ -26,16 +32,8 @@ loadData(){
     {id: 1, viewValue:'Monywa'},
     {id:2, viewValue: 'Yangon'},
     {id:3, viewValue: 'Mandalay'},
-    {id:3, viewValue: 'Shwebo'}
+    {id:4, viewValue: 'Shwebo'}
   ];
-  
-
-  showStore: boolean = false;
-  sellbuy = ['Sell', 'Buy']
-  detailForm: FormGroup;
-  showProgress:boolean=false;
-  mainFormData:SignupModel;
-  signupData: User;
   constructor(
     private authService: AuthService,
     private router: Router,
@@ -45,13 +43,13 @@ loadData(){
     ngOnInit() {
 
       this.detailForm = new FormGroup({
-        'firstname': new FormControl(null, Validators.required),
-        'lastname': new FormControl(null, Validators.required),
-        'city': new FormControl(null, Validators.required),
-        'address': new FormControl(null, Validators.required),
-        'Phone': new FormControl(null, [Validators.required, Validators.maxLength(12)]),
-        'storename': new FormControl(null, Validators.required),
-        'sell':new FormControl(null),
+        'firstname': new FormControl(null, [Validators.required,Validators.pattern('^[a-zA-Z]+$')]),
+        'lastname': new FormControl(null, [Validators.required,Validators.pattern('^[a-zA-Z]+$')]),
+        'city': new FormControl(null, [Validators.required]),
+        'address': new FormControl(null,[Validators.required]),
+        'Phone': new FormControl(null, [Validators.required, Validators.maxLength(11)]),
+        'storename': new FormControl(null),
+        'sell':new FormControl(null,[Validators.required]),
       });
 
       this.mainFormData=this.emitterService.getData();
@@ -66,26 +64,25 @@ loadData(){
   onSubmit() {
     const formValue = this.detailForm.value;
     const user_name = formValue.firstname + formValue.lastname;
-
-    this.signupData = {
-      user_name: user_name,
-      user_email: this.mainFormData.user_email,
-      user_password: this.mainFormData.user_password,
-      user_role: 1,
-      user_phone: formValue.Phone,
-      address: formValue.address,
-      storename: formValue.storename,
-      city_id: 1,
-    }
-    // if (this.user_email, this.user_password) {
-    //   this.loading = true;
-    //   this.userSrv.login(this.user.email, this.user.password); //api call here
-    //   this.loading = false;
-    // console.log(formValue);
+		if (this.detailForm.valid) {
+      this.signupData = {
+        user_name: user_name,
+        user_email: this.mainFormData.user_email,
+        user_password: this.mainFormData.user_password,
+        user_role: formValue.sell,
+        user_phone: formValue.Phone,
+        address: formValue.address,
+        storename: formValue.storename,
+        city_id: formValue.city,
+      }
+      this.emitterService.setData(this.signupData);
+      this.router.navigate(['/dashboard/dashboard']);
+   }   else {
+			return;
+		}
 
     this.authService.signup(this.signupData)
       .then((registered: boolean) => {
-        // this.loading=true;
         if (registered) {
           this.router.navigate(['/dashboard/dashboard']);
         } else {
@@ -95,8 +92,7 @@ loadData(){
       .catch((error) => {
         console.log(error);
       });
-  }
-
+  } 
 
   onSelect(event: any) {
     this.showStore = true;
