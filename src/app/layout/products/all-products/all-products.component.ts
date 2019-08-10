636381ccdc36d2e8709b9bd0a3907c7072cc5ad4
PannEiPhyuu <panneiphyu258@gamil.com>
2019-08-10 16:13:11 +0630

    json to excel

--- src/app/layout/products/all-products/all-products.component.ts
+++ src/app/layout/products/all-products/all-products.component.ts
@@ -10,6 +10,7 @@ import { EditProductComponent } from '../all-products/edit-product/edit-product.
 import { DeleteTagService } from 'src/app/delete-tag.service';
 import { ItemTag } from './deletetag/deletetag.component';
 import { Category } from 'src/app/share/models/Category';
+import { ExcelService } from 'src/app/excel.service';
 
 @Component({
   selector: 'app-all-products',
@@ -22,13 +23,12 @@ export class AllProductsComponent implements OnInit {
   
   displayedColumns: string[] = ['product_id', 'product_name', 'category_id','tag_id','price_sell'];
   
-  dataSource:any;
-  categories:any;
-  selected_category:any;
+  dataSource: any;
+  categories: any;
+  tags: any;
+  data: any;
   tag: string[]=['3 buy 1 gift','for all','for 18+'];
-  quantity: string[]=['less than 10','less than 20','less than 30','more than 50'];
-  selected:boolean=false;
-  
+  quantity: string[]=['less than 10','less than 20','less than 30','more than 50'];  
   
   @ViewChild(MatTable) table: MatTable<any>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -36,20 +36,14 @@ export class AllProductsComponent implements OnInit {
     private router: Router,
     private dataFetchService : DataFetchService,
     public dialog: MatDialog,
-    public edit:DeleteTagService
+    public edit:DeleteTagService,
+    private excelService: ExcelService
     ){ }
 
   ngOnInit() {
     this.fetchData();
-    this.selected_category=this.fetchData();
-  }
-  
-
-  getSelected() {
-    this.selected_category = this.dataSource.filter(s => {
-      return s.selected;
-    });
-    console.log(this.selected_category);
+    this.fetchCategoryData();
+    this.fetchTagData();
   }
 
     onEdit(action, obj):void {
@@ -83,7 +77,7 @@ export class AllProductsComponent implements OnInit {
       this.dataFetchService.getAllProduct().subscribe(
         response => {
           console.log(response);
-          this.categories=response;
+          this.data = response;
           this.dataSource = new MatTableDataSource<Product>(response);
           this.dataSource.paginator = this.paginator;
         },
@@ -93,6 +87,34 @@ export class AllProductsComponent implements OnInit {
       )
     }
 
+    fetchCategoryData() {
+      this.dataFetchService.getAllCategory().subscribe(
+        response => {
+          console.log(response);
+          this.categories = response;
+        },
+        error => {
+
+        }
+      )
+    }
+
+    fetchTagData() {
+      this.dataFetchService.getAllTag().subscribe(
+        response => {
+          console.log(response);
+          this.tags = response;
+        },
+        error => {
+
+        }
+      )
+    }
+
+    exportAsXLSX():void {
+      this.excelService.exportAsExcelFile(this.data, 'products');
+    }
+    
     productDetail(id: number) {
       this.router.navigate(['/dashboard/products/detailproduct',id]);
     }
