84a70e0bd327d525adf5838f27257f0d3227eb9d
myanmaronlinepos <myanmaronlinepos@gmail.com>
2019-08-11 16:29:40 +0630

    all product

--- src/app/layout/products/all-products/all-products.component.ts
+++ src/app/layout/products/all-products/all-products.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit ,ViewChild} from '@angular/core';
-import {MatPaginator} from '@angular/material';
-import {MatTableDataSource, MatTable} from '@angular/material/table';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material';
+import { MatTableDataSource, MatTable } from '@angular/material/table';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { DataSource } from '@angular/cdk/table';
 import { DataFetchService } from 'src/app/share/services/data-fetch.service';
@@ -19,82 +19,121 @@ import { Category } from 'src/app/share/models/Category';
   ]
 })
 export class AllProductsComponent implements OnInit {
-  
-  displayedColumns: string[] = ['product_id', 'product_name', 'category_id','tag_id','price_sell'];
-  
-  dataSource:any;
-  categories:any;
-  selected_category:any;
-  tag: string[]=['3 buy 1 gift','for all','for 18+'];
-  quantity: string[]=['less than 10','less than 20','less than 30','more than 50'];
-  selected:boolean=false;
-  
-  
+
+  displayedColumns: string[] = ['product_id', 'product_name', 'category_id', 'tag_id', 'price_sell'];
+
+  dataSource: any;
+  categories: any;
+  allproduct:any;
+  selected_category: any=[];
+  category_filtr:any=[];
+  tag: string[] = ['3 buy 1 gift', 'for all', 'for 18+'];
+  quantity: string[] = ['less than 10', 'less than 20', 'less than 30', 'more than 50'];
+  selected: boolean = false;
+
+
   @ViewChild(MatTable) table: MatTable<any>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(
     private router: Router,
-    private dataFetchService : DataFetchService,
+    private dataFetchService: DataFetchService,
     public dialog: MatDialog,
-    public edit:DeleteTagService
-    ){ }
+    public edit: DeleteTagService
+  ) { }
 
   ngOnInit() {
     this.fetchData();
-    this.selected_category=this.fetchData();
   }
-  
 
-  getSelected() {
-    this.selected_category = this.dataSource.filter(s => {
-      return s.selected;
+
+  getSelected(checkbox) {
+    if (!this.selected_category.includes(checkbox)) {
+      this.selected_category.push(checkbox);
+      console.log(this.selected_category);
+    } else {
+
+      const index = this.selected_category.indexOf(checkbox);
+      this.selected_category.splice(index, 1);
+    }
+    this.bindData();
+  }
+
+  bindData() {
+    this.category_filtr=[];
+    this.selected_category.forEach(item => {
+       const result = this.allproduct.filter(element => {
+       return element.category_name === item.category_name;
+       });
+       console.log("result.....");
+       console.log(result);
+       this.category_filtr=this.category_filtr.concat(result);
     });
-    console.log(this.selected_category);
+
+    console.log("length="+this.selected_category.length);
+    if(this.selected_category.length>0) {
+      this.dataSource = new MatTableDataSource<Product>(this.category_filtr);
+      this.dataSource.paginator = this.paginator;
+      return;
+    }
+    
+    this.dataSource = new MatTableDataSource<Product>(this.allproduct);
+    this.dataSource.paginator = this.paginator;
+  }
+  onEdit(action, obj): void {
+
+    // this.categoryservice.setCategory(row);
+    obj.action = action;
+    const dialogRef = this.dialog.open(EditProductComponent, {
+      width: '900px',
+      height: '1300px',
+      data: obj
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result.event == 'Edit') {
+        this.editRowData(result.data);
+      }
+    });
+  }
+  editRowData(row_obj) {
+    this.dataSource = this.dataSource.filter((value, key) => {
+      if (value.id == row_obj.id) {
+        value.product_name = row_obj.product - name;
+        value.category_id = row_obj.category_id;
+        value.tag_id = row_obj.tag_id;
+
+      }
+      return true;
+    });
   }
 
-    onEdit(action, obj):void {
+  fetchData() {
+
+    //fetch all category data
+    this.dataFetchService.getAllCategory().subscribe(
+      response => {
+        this.categories=response;
+      },
+      error => {
+        console.log(error);
+      }
+    )
+
+    // fetch all product data
+    this.dataFetchService.getAllProduct().subscribe(
+      response => {
     
-      // this.categoryservice.setCategory(row);
-      obj.action=action;
-      const dialogRef=this.dialog.open(EditProductComponent,{
-         width: '900px',
-         height: '1300px',
-         data: obj
-      });
-      dialogRef.afterClosed().subscribe(result=> {
-        if(result.event=='Edit') {
-          this.editRowData(result.data);
-        }
-      });
-    }
-    editRowData(row_obj) {
-      this.dataSource = this.dataSource.filter((value,key)=>{
-       if(value.id == row_obj.id){
-         value.product_name=row_obj.product-name;
-         value.category_id =row_obj.category_id;
-         value.tag_id=row_obj.tag_id;
-    
-       }
-       return true;
-      });
-    }
+        this.allproduct=response;
+        this.dataSource = new MatTableDataSource<Product>(this.allproduct);
+        this.dataSource.paginator = this.paginator;
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
 
-    fetchData() {
-      this.dataFetchService.getAllProduct().subscribe(
-        response => {
-          console.log(response);
-          this.categories=response;
-          this.dataSource = new MatTableDataSource<Product>(response);
-          this.dataSource.paginator = this.paginator;
-        },
-        error => {
+  productDetail(id: number) {
+    this.router.navigate(['/dashboard/products/detailproduct', id]);
+  }
 
-        }
-      )
-    }
-
-    productDetail(id: number) {
-      this.router.navigate(['/dashboard/products/detailproduct',id]);
-    }
-  
 }
