eee20c59f6f05d26c6a9d01ae65490abd4c2d5f1
PannEiPhyuu <panneiphyu258@gamil.com>
2019-08-14 16:49:46 +0630

    detail product

--- src/app/layout/products/detail-product/detail-product.component.ts
+++ src/app/layout/products/detail-product/detail-product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
+import {MatTableDataSource, MatTable} from '@angular/material/table';
 import { MatPaginator } from '@angular/material';
 import { ActivatedRoute, Params } from '@angular/router';
 import { DataFetchService } from 'src/app/share/services/data-fetch.service';
@@ -12,13 +12,14 @@ import { Product } from 'src/app/share/models/Product';
 })
 export class DetailProductComponent implements OnInit {
 
-  displayedColumns: string[] = ['date', 'before', 'addQuantity', 'sellQuantity', 'after','cost','sale'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns: string[] = ['date', 'product_name', 'quantity', 'price_cost', 'price_sell', 'total_cost', 'total_sell', 'profit'];
+  dataSource: any;
   id: string;
   allproduct: any;
   products: any;
   product: Product;
 
+  @ViewChild(MatTable) table: MatTable<any>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(
     private route:ActivatedRoute,
@@ -26,13 +27,13 @@ export class DetailProductComponent implements OnInit {
   ) {}
  
   ngOnInit() {
-    this.dataSource.paginator = this.paginator;
     this.route.params.subscribe(
       (param:Params) => {
         this.id = param['product_id'];
        this.fetchData();
       }
-    )
+    );
+    this.fetchProductData();
   }
 
   fetchData() {
@@ -53,20 +54,16 @@ export class DetailProductComponent implements OnInit {
     )
   }
 
-}
+  fetchProductData() {
+    this.dataFetchService.getSellHistory().subscribe(
+      response => {
+        this.dataSource = new MatTableDataSource<any>(response);
+        this.dataSource.paginator = this.paginator;
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
 
-export interface PeriodicElement {
-  date: string;
-  before: number;
-  addQuantity: number;
-  sellQuantity: number;
-  after: number;
-  cost: number;
-  sale: number;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-{ date: '1/7/2019', before:1000, addQuantity:10, sellQuantity:5, after:1005, cost:500, sale:600},
-{ date: '1/7/2019', before:1000, addQuantity:10, sellQuantity:5, after:1005, cost:500, sale:600},
-{ date: '1/7/2019', before:1000, addQuantity:10, sellQuantity:5, after:1005, cost:500, sale:600}
-];
